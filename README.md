5c5a396e462adf80d38ee35fd9be9a4c76d57946
Taha Samadi <taha.samadi@gmail.com>
2026-08-06 17:39:47 +0330

    chore: initialize Flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# movie_tracker_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/ir/sharifmp/movie_tracker_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package ir.sharifmp.movie_tracker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
